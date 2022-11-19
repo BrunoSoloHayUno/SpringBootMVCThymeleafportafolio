8dc0bfa72beaa920c91e99e95017f31c077b7fd4
Christian Cachero <Ch_canchero@live.com.ar>
2022-11-19 19:02:05 -0300

    comiteando

---
+++ SpringBootMVCPortafolio/src/main/java/com/portafolio/projects/SpringBootMVCPortafolio/SpringBootMvcPortafolioApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.projects.SpringBootMVCPortafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcPortafolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcPortafolioApplication.class, args);
+	}
+
+}
---
+++ SpringBootMVCPortafolio/src/test/java/com/portafolio/projects/SpringBootMVCPortafolio/SpringBootMvcPortafolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.projects.SpringBootMVCPortafolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcPortafolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
